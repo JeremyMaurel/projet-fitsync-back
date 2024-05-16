c8aa08243935629872b7e58f9721c63cd9d4c9c2
Boyer Jennifer <jennifer.boyer@oclock.school>
2024-05-16 12:34:45 +0200

    chore: set up environment-specific configurations

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+/* eslint-disable no-underscore-dangle */
 import debugMe from 'debug';
-import 'dotenv/config';
+import dotenv from 'dotenv';
 import express from 'express';
+import path from 'path';
+
+import { fileURLToPath } from 'url';
 import router from './app/routers/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config({ path: path.resolve(__dirname, `.env.${process.env.NODE_ENV}`) });
 
 const debug = debugMe('app:server');
 
