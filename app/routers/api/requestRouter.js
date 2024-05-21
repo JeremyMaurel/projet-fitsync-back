71f7d6cfd1183943006dc5c288440cb2152b1c54
Boyer Jennifer <jennifer.boyer@oclock.school>
2024-05-21 13:58:46 +0200

    chore: add comments

--- app/routers/api/requestRouter.js
+++ app/routers/api/requestRouter.js
@@ -10,6 +10,18 @@ const { userDatamapper } = datamappers;
 
 const router = Router();
 
+/**
+ * @route POST /requests
+ * @summary Create a new request.
+ * @tags Requests
+ * @param {object} request.body.required - The request body - application/json
+ * @param {string} request.body.user_id - The ID of the user
+ * @param {string} request.body.some_other_field - Another field required for the request
+ * @return {object} 201 - Success response - application/json
+ * @return {ApiJsonError} 400 - Bad request response - application/json
+ * @return {ApiJsonError} 404 - Not Found - application/json
+ * @return {ApiJsonError} 500 - Internal Server Error - application/json
+ */
 router.post('/requests', validator(requestsCreateSchema, 'body'), checkById([{ modelInstance: userDatamapper, idKey: 'user_id', entityName: 'User' }]), cw(RequestController.create.bind(RequestController)));
 
 export default router;
--- app/schemas/middleware/checkById.js
+++ app/schemas/middleware/checkById.js
@@ -1,5 +1,16 @@
 import ApiError from '../../errors/apiError.js';
 
+/**
+ * Middleware to check the existence of entities by their IDs.
+ * This middleware takes an array of checks, where each check specifies the model instance,
+ * the ID key to look for in the request, and the entity name.
+ * It validates the existence of these entities in the database.
+ * @param {Array} checks - An array of check objects.
+ * @param {Object} checks[].modelInstance - The instance of the model to use for checking existence.
+ * @param {string} checks[].idKey - The key to look for in the request to get the ID (ex:'user_id').
+ * @param {string} checks[].entityName - The name of the entity to check (e.g., 'User').
+ * @returns {Function} Middleware function to use in Express routes.
+ */
 const checkById = (checks) => async (req, res, next) => {
   try {
     const promises = checks.map(async (check) => {
