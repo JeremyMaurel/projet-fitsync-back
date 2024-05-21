c6b1b5481107286d736dd40c51d37ea24852fa19
Jeremy Maurel <jeremy.maurel@oclock.school>
2024-05-21 10:43:33 +0200

    feat: path post/session without schema ok

--- app/routers/api/sessionRouter.js
+++ app/routers/api/sessionRouter.js
@@ -30,4 +30,21 @@ router.get('/sessions-history', cw(sessionController.getAllSessionWithActivities
  */
 router.delete('/session-history', cw(sessionController.deleteSession.bind(sessionController)));
 
+/**
+ * @route POST /session
+ * @summary Creates a new session with the provided data
+ * @tags Sessions
+ * @param {integer} request.body.duration.required - The duration of the session in minutes
+ * @param {string} request.body.date.required - The date and time of the session in the format 'YYYY-MM-DD HH:mm:ss+TZ'
+ * @param {string} request.body.comment - An optional comment about the session
+ * @param {integer} request.body.user_id.required - The ID of the user associated with the session
+ * @param {integer} request.body.activity_id.required - The ID of the activity associated with the session
+ * @return {201} 201 - Created - Successfully created the session
+ * @return {400} 400 - Bad Request - Invalid session data provided
+ * @return {401} 401 - Unauthorized - Invalid or missing authorization token
+ * @return {404} 404 - Not Found - User or activity not found
+ * @return {500} 500 - Internal Server Error - Unexpected error
+ */
+router.post('/session', cw(sessionController.create.bind(sessionController)));
+
 export default router;
---
+++ app/schemas/session.js
@@ -0,0 +1,5 @@
+// "duration" INTEGER NOT NULL,
+// "date" TIMESTAMPTZ NOT NULL,
+// "comment" VARCHAR(1024),
+// "user_id" INT NOT NULL REFERENCES "user"("id") ON DELETE CASCADE,
+// "activity_id"
