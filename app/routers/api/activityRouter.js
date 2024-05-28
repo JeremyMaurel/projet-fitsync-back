056cee7be0200a26ff2894f8cee07c63734f7ef1
Jeremy Maurel <jeremy.maurel@oclock.school>
2024-05-28 18:24:03 +0200

    docs: swagger documentation ok for activities

--- app/routers/api/activityRouter.js
+++ app/routers/api/activityRouter.js
@@ -30,50 +30,74 @@ const router = Router();
  * @param {number} id.path.required - The ID of the activity to retrieve
  * @return {Activity} 200 - Success response - application/json
  * @return {ApiJsonError} 400 - Bad request response - application/json
- * @return {ApiJsonError} 401 - Unauthorized - JWT not provided or invalid - application/json
+ * @return {ApiJsonError} 401 - Unauthorized: JWT not provided or invalid - application/json
  * @return {ApiJsonError} 404 - Not found response - application/json
  * @return {ApiJsonError} 500 - Internal Server Error - application/json
  */
 router.get('/activities/:id', cw(ActivityController.getOne.bind(ActivityController)));
 
 /**
+ * @typedef {object} Data
+ * @property {Activity} activity - an activity
+ *
+ */
+/**
+ * @typedef {object} Activities
+ * @property {number} total - total of activities
+ * @property {Data} data - an array containing the activities
+ *
+ */
+
+/**
  * GET /api/v1/activities
  * @summary Get all activities
  * @tags Activities
- * @return {Activity} 200 - Success response - application/json
+ * @return {Activities} 200 - Success response - application/json
  * @return {ApiJsonError} 400 - Bad request response - application/json
- * @return {ApiJsonError} 401 - Unauthorized - JWT not provided or invalid - application/json
+ * @return {ApiJsonError} 401 - Unauthorized: JWT not provided or invalid - application/json
  * @return {ApiJsonError} 404 - Not found response - application/json
  * @return {ApiJsonError} 500 - Internal Server Error - application/json
  */
 router.get('/activities', cw(ActivityController.getAll.bind(ActivityController)));
 
 /**
+ * @typedef {object} newActivity
+ * @property {string} name - The name of the activity
+ * @property {number} met - The MET value of the activity
+ * @property {number} category_id - The ID of the category this activity belongs to
+ */
+
+/**
  * POST /api/v1/activities
  * @summary Create a new activity
  * @tags Activities
  * @security BearerAuth
- * @param {string} authorization.header.required - Bearer token for authorization
- * @param {object} request.body.required - The activity data - application/json
- * @param {string} request.body.name - The name of the activity
- * @param {number} request.body.met - The MET per minuts of the activity
- * @param {number} request.body.categoryId - The categoryId of the activity
- * @return {Activity} 200 - OK - Successfully logged in with JWT
+ * @param {newActivity} request.body.object.required - The new activity data - application/json
+ * @param {string} name.string.required - The name of the activity
+ * @param {number} met.number.required - The MET per minuts of the activity
+ * @param {number} categoryId.number.required - The categoryId of the activity
+ * @return {Activity} 200 - OK Successfully logged in with JWT - application/Json
  * @return {ApiJsonError} 400 - Bad Request - application/json
  * @return {ApiJsonError} 500 - Internal Server Error - application/json
  */
 router.post('/activities', validateToken, isAdmin, validator(activityCreateSchema, 'body'), cw(ActivityController.createActivityByCategoryId.bind(ActivityController)));
 
 /**
+ * @typedef {object} PatchActivity
+ * @property {string} name - The name of the activity
+ * @property {number} met - The MET value of the activity
+ * @property {number} category_id - The ID of the category this activity belongs to
+ */
+/**
  * PATCH api/v1/activities/{id}
  * @summary Update a new activity
  * @tags Activities
  * @security BearerAuth
- * @param {string} authorization.header.required - Bearer token for authorization
- * @param {object} request.body.required - The activity data - application/json
- * @param {string} request.body.name - The name of the activity
- * @param {number} request.body.met - The MET per minuts of the activity
- * @param {number} request.body.categoryId - The categoryId of the activity
+ * @param {number} id.path.required - The ID of the activity to patch
+ * @param {PatchActivity} request.body.required - The patched activity data - application/json
+ * @param {string} name.string - The name of the activity
+ * @param {number} met.number - The MET per minuts of the activity
+ * @param {number} categoryId.number - The categoryId of the activity
  * @return {Activity} 200 - OK - Successfully logged in with JWT
  * @return {ApiJsonError} 400 - Bad Request - application/json
  * @return {ApiJsonError} 500 - Internal Server Error - application/json
@@ -85,9 +109,9 @@ router.patch('/activities/:id', validateToken, isAdmin, validator(activityUpdate
  * @summary Delete a new activity
  * @tags Activities
  * @security BearerAuth
- * @param {string} authorization.header.required - Bearer token for authorization
+ * @param {number} id.path.required - The ID of the activity to delete
  * @return {void} 204 - No Content - Successfully deleted the activity
- * @return {ApiJsonError} 401 - Unauthorized - Invalid or missing token
+ * @return {ApiJsonError} 401 - Unauthorized:- Invalid or missing token
  * @return {ApiJsonError} 404 - Not Found - Activity entry not found
  * @return {ApiJsonError} 500 - Internal Server Error - Unexpected error
 */
